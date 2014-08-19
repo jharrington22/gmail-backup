464c841c6a1bafdcd0314890e45a817580c5eed9
jharrington22 <james@harrington.net.au>
2014-08-19 17:27:29 +1000

    --backup and --status functionality now working

--- google-apps-backup.py
+++ google-apps-backup.py
@@ -18,8 +18,10 @@ parser.add_argument("-t", "--token", help="Google Apps authorization token")
 parser.add_argument("-e", "--email", help="Google Apps user email address")
 parser.add_argument("-k", "--key", help="GPG Public key file")
 parser.add_argument("-i", "--installKey", help="Install new Public GPG file (created with GPG tool)")
-parser.add_argument("-s", "--status", help="Check status of request and download if complete (downloads to /tmp)")
-parser.add_argument("-r", "--requestId", help="Request Id of mbox retrival request")
+parser.add_argument("-s", "--status", action="store_true", help="Check status of request and download if complete (downloads to /tmp)")
+parser.add_argument("-r", "--requestId", help="Request Id of mbox request")
+parser.add_argument("-b", "--backup", action="store_true", help="Request download of mbox")
+
 args = parser.parse_args()
 
 epoch = str(time.time()).split('.')[0]
@@ -38,6 +40,10 @@ mailboxExportStatusUrl = "https://apps-apis.google.com/a/feeds/compliance/audit/
 
 # Get users home directory
 homeDirectory = os.path.expanduser("~") + "/"
+# Set default cache file location
+cacheFileLocation = homeDirectory + '.google-apps-backup.json'
+
+
 
 def formatPublicGpgUpload(publicGpgStringFile):
     """Read GPG public key from file and format Atom XML request for POST"""
@@ -72,62 +78,132 @@ def getAuthTokenParams(username, password):
     return params
 
 
-def requestMailboxDownload(url, token):
+def requestMailboxDownload(url, cacheFile):
     """Sets up request to download mailbox data"""
+    success = 0
     postData = formatMailboxDownloadRequestXml()
-    print postData
-    responseData = postToGoogle(publicKeyUploadHeader, postData, url, token)
-    print responseData
+    responseData = postToGoogle(publicKeyUploadHeader, postData, url, cacheFile["token"]["Auth"])
+    if not responseData == False:
+        tree = etree.parse(responseData)
+        for elem in tree.iterfind('{http://schemas.google.com/apps/2006}property'):
+            try:
+                if elem.attrib["name"] == "requestId":
+                    print "Request Id: %s" % elem.attrib["value"]
+                    requestId = elem.attrib["value"]
+                    success = 1
+            except KeyError:
+                pass
+            try:
+                if elem.attrib["name"] == "userEmailAddress":
+                    print "Requested Users Mailbox: %s" % elem.attrib["value"]
+                    requestedMailbox = elem.attrib["value"]
+                    success = 1
+            except KeyError:
+                pass
+            try:
+                if elem.attrib["name"] == "adminEmailAddress":
+                    print "Admin Email of Requester: %s" % elem.attrib["value"]
+                    adminEmail = elem.attrib["value"]
+                    success = 1
+            except KeyError:
+                pass
+            try:
+                if elem.attrib["name"] == "status":
+                    print "Status of request: %s" % elem.attrib["value"]
+                    statusOfRequest = elem.attrib["value"]
+                    success = 1
+            except KeyError:
+                pass
+            try:
+                if elem.attrib["name"] == "requestDate":
+                    print "Request Date: %s" % elem.attrib["value"]
+                    requestDate = elem.attrib["value"]
+                    success = 1
+            except KeyError:
+                pass
+            try:
+                if elem.attrib["name"] == "packageContent":
+                    print "Package Contents: %s" % elem.attrib["value"]
+                    packageContents = elem.attrib["value"]
+                    success = 1
+            except KeyError:
+                pass
+        if success == 1:
+            # Write request response to cacheFile
+            if not "requests" in cacheFile:
+                cacheFile["requests"] = {}
+            cacheFile["requests"][requestId] = {"requestedMailbox": requestedMailbox, "adminEmail": adminEmail, "status": statusOfRequest, "requestDate": requestDate, "packageContents": packageContents}
+            with open(cacheFileLocation, 'w') as cacheFileFp:
+                cacheFileFp.write(json.dumps(cacheFile))
 
 
-def getMailboxExportStatus(url, requestId, authToken, email, epoch):
+def getMailboxExportStatus(url, cacheFile, email, epoch):
     """Print mailbox status and download mbox file if status is Complete"""
-    status = getFromGoogle(requestId, url, authToken)
-    tree = etree.parse(status)
-    for elem in tree.iterfind('{http://schemas.google.com/apps/2006}property'):
-        try:
-            if elem.attrib["name"] == "requestId":
-                print "Request Id: %s" % elem.attrib["value"]
-        except KeyError:
-            pass
-        try:
-            if elem.attrib["name"] == "status":
-                print "Status of request: %s" % elem.attrib["value"]
-        except KeyError:
-            pass
-        try:
-            if elem.attrib["name"] == "fileUrl0":
-                print "Downloading file 0 for mbox: %s" % elem.attrib["value"]
-                downloadMboxFile(elem.attrib["value"], email, epoch, "0")
-        except KeyError:
-            pass
-        try:
-            if elem.attrib["name"] == "fileUrl1":
-                print "Downloading file 1 for mbox: %s" % elem.attrib["value"]
-                downloadMboxFile(elem.attrib["value"], email, epoch, "1")
-        except KeyError:
-            pass
+    for request in cacheFile["requests"]:
+        print("Checking status of request %s.." % request)
+        if cacheFile["requests"][request]["status"] == "PENDING":
+            status = getFromGoogle(request, url, cacheFile["token"]["Auth"])
+            if not status == False:
+                tree = etree.parse(status)
+                for elem in tree.iterfind('{http://schemas.google.com/apps/2006}property'):
+                    try:
+                        if elem.attrib["name"] == "requestId":
+                            print "Request Id: %s" % elem.attrib["value"]
+                    except KeyError:
+                        pass
+                    try:
+                        if elem.attrib["name"] == "requestDate":
+                            print "Request Date: %s" % elem.attrib["value"]
+                    except KeyError:
+                        pass
+                    try:
+                        if elem.attrib["name"] == "completedDate":
+                            print "Complete Date: %s" % elem.attrib["value"]
+                    except KeyError:
+                        pass
+                    try:
+                        if elem.attrib["name"] == "status":
+                            print "Status of request: %s" % elem.attrib["value"]
+                    except KeyError:
+                        pass
+                    try:
+                        if elem.attrib["name"] == "fileUrl0":
+                            print "Downloading file 0 for mbox: %s" % elem.attrib["value"]
+                            downloadMboxFile(elem.attrib["value"], email, epoch, "0")
+                    except KeyError:
+                        pass
+                    try:
+                        if elem.attrib["name"] == "fileUrl1":
+                            print "Downloading file 1 for mbox: %s" % elem.attrib["value"]
+                            downloadMboxFile(elem.attrib["value"], email, epoch, "1")
+                    except KeyError:
+                        pass
+            else:
+                print("Error retrieving status for request: %s" % request)
+            status = None
+            tree = None
+        else:
+            print("Already Complete")
 
 
 def postToGoogle(contentType, data, url, authorization=None):
-    """POST data Google """
+    """POST data Google"""
     req = urllib2.Request(url)
     req.add_header('Content-Type', contentType)
     if not authorization == None:
         req.add_header('Authorization', "GoogleLogin auth=%s" % authorization)
-    print req.headers
     try:
         response = urllib2.urlopen(req, data)
-        return response.read().rstrip()
+        return response
     except Exception as e:
         print e
+        print "Error posting data to Google"
         return False
 
 
 def getFromGoogle(requestId, url, authorization=None):
     """GET data Google """
     url = url + requestId
-    print url
     req = urllib2.Request(url)
     if not authorization == None:
         req.add_header('Authorization', "GoogleLogin auth=%s" % authorization)
@@ -136,6 +212,7 @@ def getFromGoogle(requestId, url, authorization=None):
         return response
     except Exception as e:
         print e
+        print "Error in GET request to Google"
         return False
 
 
@@ -155,30 +232,32 @@ def downloadMboxFile(url, email, epoch, num):
 
 # TODO: Finish if's below, currently works if you run auth then install then request then status until mailbox is ready
 
-if args.auth:
-    # Build URL and request auth Token (Only valid for 24 hours)
-    # TODO: Check if cache file is older than 24 hours and generate a new token
-    # postToGoogle responds with three long alphanumeric codes SID,LSID and Auth
-    # SID = Session ID
-    # LSID = Is for APIs not using the Google Data protocol
-    # Auth = Authorization token the we'll send to the email audit service
-    # Store these in a cache file as they are valid for 24 hours
-    cacheFileLocation = homeDirectory + '.google-apps-backup.json'
-    if os.path.exists(cacheFileLocation):
-        with open(cacheFileLocation, 'r') as cacheFileFp:
-            cacheFile = json.load(cacheFileFp)
-            authToken = cacheFile["token"]["Auth"]
-            print cacheFile["token"]["Auth"]
-    else:
-        urlParams = getAuthTokenParams(args.username, args.password)
-        authToken = postToGoogle(tokenRequestHeader, urlParams, clientLoginUrl)
-        SID, LSID, Auth = authToken.split("\n")
-        SID = SID.split("=")[1]
-        LSID = LSID.split("=")[1]
-        Auth = Auth.split("=")[1]
-        authToken = {"token": {"SID": SID, "LSID": LSID, "Auth": Auth}}
-        with open(cacheFileLocation, 'w') as cacheFile:
-            cacheFile.write(json.dumps(authToken))
+# If token is specified set authToken
+if args.token:
+    authToken = args.token
+
+# Build URL and request auth Token (Only valid for 24 hours)
+# TODO: Check if cache file is older than 24 hours and generate a new token
+# postToGoogle responds with three long alphanumeric codes SID,LSID and Auth
+# SID = Session ID
+# LSID = Is for APIs not using the Google Data protocol
+# Auth = Authorization token the we'll send to the email audit service
+# Store these in a cache file as they are valid for 24 hours
+if os.path.exists(cacheFileLocation):
+    with open(cacheFileLocation, 'r') as cacheFileFp:
+        cacheFile = json.load(cacheFileFp)
+        authToken = cacheFile["token"]["Auth"]
+else:
+    urlParams = getAuthTokenParams(args.username, args.password)
+    authToken = postToGoogle(tokenRequestHeader, urlParams, clientLoginUrl)
+    SID, LSID, Auth = authToken.read().rstrip().split("\n")
+    SID = SID.split("=")[1]
+    LSID = LSID.split("=")[1]
+    Auth = Auth.split("=")[1]
+    cacheFile = {"token": {"SID": SID, "LSID": LSID, "Auth": Auth}}
+    authToken = cacheFile["token"]["Auth"]
+    with open(cacheFileLocation, 'w') as cacheFileFp:
+        cacheFileFp.write(json.dumps(cacheFile))
 
 if args.installKey:
     # Insert new public GPG key to Google Apps
@@ -188,13 +267,12 @@ if args.installKey:
     #print publicKeyResponse
     pass
 
-if args.requestId:
-    print requestMailboxDownload(mailboxExportUrl, authToken)
-    pass
+if args.backup:
+    # From this request need to pickup;
+    # <apps:property name='requestId' value='470444354'/>
+    # This value then needs to be used in getMailboxExportStatus
+    requestMailboxDownload(mailboxExportUrl, cacheFile)
 
 if args.status:
     # Check mailbox status download if status is Complete
-    #getMailboxExportStatus(mailboxExportStatusUrl, "427823502", args.token, args.email, epoch)
-    pass
-
-#print requestMailboxDownload(mailboxExportUrl, authToken)
+    getMailboxExportStatus(mailboxExportStatusUrl, cacheFile, args.email, epoch)
